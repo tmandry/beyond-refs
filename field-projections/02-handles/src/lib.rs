17afe22ce97a9f0aedf597cdb4bef962d553dc49
Benno Lossin <benno.lossin@proton.me>
2026-05-30 10:10:03 +0200

    enable unstable features for enum support

--- field-projections/02-handles/src/lib.rs
+++ field-projections/02-handles/src/lib.rs
@@ -1,5 +1,9 @@
+#![expect(incomplete_features)]
 // Needed for proper support of `?Sized` types.
 #![feature(ptr_metadata)]
+// Needed for enum support (`&'static str` in const generics).
+#![feature(adt_const_params)]
+#![feature(unsized_const_params)]
 
 pub mod application;
 pub mod design;
