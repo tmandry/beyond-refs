8c1603911f2edf547a4e4a6a4f71323d60f7872d
Benno Lossin <benno.lossin@proton.me>
2026-06-09 13:11:13 +0200

    add `RefHandle` for `&T`

--- field-projections/02-handles/src/application/refs.rs
+++ field-projections/02-handles/src/application/refs.rs
@@ -2,7 +2,7 @@ use std::{marker::PhantomData, ptr::NonNull};
 
 use crate::design::{
     borrowck::{AccessKind, Instant, Lifetime},
-    ops::{BorrowPlace, DerefHandle, DerefPlace, PlaceHandle, ProjectPlace, ProxyPlace},
+    ops::{BorrowPlace, DerefHandle, DerefPlace, PlaceHandle, ProjectPlace, ProxyPlace, ReadPlace},
     subplace::Subplace,
 };
 
@@ -67,5 +67,79 @@ where
 
     unsafe fn borrow(self) -> &'b mut T {
         unsafe { &mut *self.ptr.as_ptr() }
+    }
+}
+
+pub struct RefHandle<'a, T: ?Sized> {
+    ptr: NonNull<T>,
+    _lt: PhantomData<&'a mut T>,
+}
+
+impl<'a, T: ?Sized> ProxyPlace for &'a T {
+    type Handle = RefHandle<'a, T>;
+}
+
+impl<'a, T: ?Sized> PlaceHandle for RefHandle<'a, T> {
+    type Target = T;
+}
+
+impl<'a, T: ?Sized> DerefHandle for &'a T {
+    const ACCESS: AccessKind = AccessKind::Exclusive;
+    type Timing = Lifetime<'a>;
+
+    unsafe fn handle_from_raw(this: *const Self) -> Self::Handle {
+        let ptr: *const *const T = this.cast::<*const T>();
+        let handle = unsafe { *ptr };
+        let handle = handle.cast_mut();
+        RefHandle {
+            ptr: unsafe { NonNull::new_unchecked(handle) },
+            _lt: PhantomData,
+        }
+    }
+}
+
+impl<'a, S: Subplace<Target: 'a>> ProjectPlace<S> for RefHandle<'a, S::Source> {
+    type Projected = RefHandle<'a, S::Target>;
+
+    unsafe fn project_place(self, subplace: S) -> Self::Projected {
+        RefHandle {
+            ptr: unsafe { self.ptr.project_place(subplace) },
+            _lt: PhantomData,
+        }
+    }
+}
+
+impl<'a, P: ?Sized> DerefPlace<P::Timing, Instant> for RefHandle<'a, P>
+where
+    P: DerefHandle,
+{
+    const POINTEE_ACCESS: AccessKind = P::ACCESS;
+    const POINTER_ACCESS: AccessKind = AccessKind::Shared;
+    const SAFE: bool = true;
+
+    unsafe fn deref_place(self) -> <Self::Target as ProxyPlace>::Handle {
+        unsafe { P::handle_from_raw(self.ptr.as_ptr()) }
+    }
+}
+
+impl<'a, 'b, T> BorrowPlace<&'b T> for RefHandle<'a, T>
+where
+    'a: 'b,
+{
+    const ACCESS: AccessKind = AccessKind::Shared;
+    type Timing = Lifetime<'b>;
+    const SAFE: bool = true;
+
+    unsafe fn borrow(self) -> &'b T {
+        unsafe { &*self.ptr.as_ptr() }
+    }
+}
+
+impl<'a, T> ReadPlace for RefHandle<'a, T> {
+    const ACCESS: AccessKind = AccessKind::Shared;
+    const SAFE: bool = true;
+
+    unsafe fn read_place(self) -> Self::Target {
+        unsafe { self.ptr.read() }
     }
 }
