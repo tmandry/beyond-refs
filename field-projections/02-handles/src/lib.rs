0cab866fdbe52340ac1b9fd10388de574fd02f22
Benno Lossin <benno.lossin@proton.me>
2026-06-10 13:12:54 +0200

    gate custom_inner_attributes behind doc

--- field-projections/02-handles/src/lib.rs
+++ field-projections/02-handles/src/lib.rs
@@ -4,7 +4,7 @@
 // Needed for enum support (`&'static str` in const generics).
 #![feature(adt_const_params)]
 #![feature(unsized_const_params)]
-#![feature(custom_inner_attributes)]
+#![cfg_attr(doc, feature(custom_inner_attributes))]
 #![feature(proc_macro_hygiene)]
 
 pub mod application;
