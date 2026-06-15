33757f12f95aa95c2b1eefb8d6099a2a48cf9fcd
Benno Lossin <benno.lossin@proton.me>
2026-06-15 15:26:56 +0200

    examples: unnormalized: remove unused feature

--- field-projections/02-handles/examples/unnormalized/main.rs
+++ field-projections/02-handles/examples/unnormalized/main.rs
@@ -1,5 +1,5 @@
 #![expect(incomplete_features, internal_features, dead_code)]
-#![feature(auto_traits, negative_impls, negative_bounds, offset_of_enum)]
+#![feature(auto_traits, negative_impls, offset_of_enum)]
 #![feature(adt_const_params)]
 #![feature(unsized_const_params)]
 
