baf9ae7239f94d01453ceb5170b00855250a5d05
Benno Lossin <benno.lossin@proton.me>
2026-07-10 10:13:42 +0200

    landing documentation

--- field-projections/design/src/lib.rs
+++ field-projections/design/src/lib.rs
@@ -1,3 +1,54 @@
+//! # *Handles* -- a Field Projection Design
+//!
+//! This crate is not only meant as a wiki and up-to-date version of the design,
+//! but also as a testing ground for new ideas. It also is very much intended to
+//! be used by people who would like to figure out if field projections can help
+//! in their use-case.
+//!
+//! <p class="warning"> This crate essentially enables writing
+//! (<code>unsafe</code>) desugared code and running it like any other Rust
+//! code. It is <strong>not</strong> designed for production, only for
+//! prototypes. It makes no guarantees on soundness, API stability, or
+//! correctness. </p>
+//!
+//! Another word of warning: writing code using this crate means manually
+//! desugaring the code that you actually wish to write; it is tedious and
+//! error-prone. But depending on your use-case, you might already be writing
+//! similarly convoluted code.
+//!
+//! ## Design Overview
+//!
+//! This design builds on top of the place centered field projection design
+//! invented by Nadrieril. The core idea that this design adds is that places
+//! are represented using *handles*. Every place expression has a corresponding
+//! handle.
+//!
+//! You can think of a handle as a pointer to the place it represents--- most of
+//! the time, that's also how it's implemented. But there also are virtual
+//! places where the handle can be a ZST. Furthermore, handles can of course
+//! contain more information than a "normal pointer", so access permissions,
+//! multiple pointers etc.
+//!
+//! ## Crate Overview
+//!
+//! This crate has the same structure as the standard library, as many parts are
+//! intended to be eventually incorporated into it. In addition, this crate also
+//! contains compatibility APIs and workarounds for language limitations that
+//! need compiler support.
+//!
+//!
+//! Here is a list of contents sorted by relevance (the most important item is
+//! the first):
+//! 1. [`ops::place`] -- all place operations that can be implemented on
+//!    handles.
+//! 2. [`place`] -- several non-operation place traits, utility types, and
+//!    handles for builtin types and local variables.
+//! 3. [`lang_limits`] -- APIs that work around compiler & language limitations,
+//!    required to write examples.
+//! 4. [`cell`], [`mem`], [`option`], [`pin`], [`ptr`], [`sync`], [`mod@vec`] --
+//!    extensions to standard library modules that integrate them into the place
+//!    operations.
+
 #![expect(incomplete_features)]
 // Needed for proper support of `?Sized` types.
 #![feature(ptr_metadata)]
