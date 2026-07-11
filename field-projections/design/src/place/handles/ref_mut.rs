1b0ee5101bfbe993f58f4dba1ee140a5d3841cfe
Benno Lossin <benno.lossin@proton.me>
2026-07-11 11:38:43 +0200

    more place ops for MutHandle

--- field-projections/design/src/place/handles/ref_mut.rs
+++ field-projections/design/src/place/handles/ref_mut.rs
@@ -8,9 +8,12 @@ use crate::{
         BorrowPlace,
         DerefHandle,
         DerefPlace,
+        DropPlace,
         PlaceHandle,
         ProjectPlace,
         ProxyPlace,
+        ReadPlace,
+        WritePlace,
         borrowck::{
             AccessKind,
             Instant,
@@ -24,6 +27,13 @@ pub struct MutHandle<'a, T: ?Sized> {
     ptr: NonNull<T>,
     _lt: PhantomData<&'a mut T>,
 }
+
+impl<T: ?Sized> Clone for MutHandle<'_, T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+impl<T: ?Sized> Copy for MutHandle<'_, T> {}
 
 impl<'a, T: ?Sized> MutHandle<'a, T> {
     pub unsafe fn from_raw(ptr: *mut T) -> Self {
@@ -53,6 +63,30 @@ unsafe impl<T: ?Sized> DerefHandle for &mut T {
             ptr: unsafe { NonNull::new_unchecked(handle) },
             _lt: PhantomData,
         }
+    }
+}
+
+unsafe impl<T> WritePlace for MutHandle<'_, T> {
+    const ACCESS: AccessKind = AccessKind::Exclusive;
+    const SAFE: bool = true;
+
+    unsafe fn write_place(self, value: Self::Target) {
+        unsafe { self.ptr.write(value) };
+    }
+}
+
+unsafe impl<T> DropPlace for MutHandle<'_, T> {
+    unsafe fn drop_place(self) {
+        unsafe { self.ptr.drop_in_place() };
+    }
+}
+
+unsafe impl<T> ReadPlace for MutHandle<'_, T> {
+    const ACCESS: AccessKind = AccessKind::Exclusive;
+    const SAFE: bool = true;
+
+    unsafe fn read_place(self) -> Self::Target {
+        unsafe { self.ptr.read() }
     }
 }
 
