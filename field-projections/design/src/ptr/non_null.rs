fc831c19c28e355faf2eee6eb4a2366434c1628f
Benno Lossin <benno.lossin@proton.me>
2026-07-20 12:57:47 +0200

    ptr: implement more place ops for `NonNull`

--- field-projections/design/src/ptr/non_null.rs
+++ field-projections/design/src/ptr/non_null.rs
@@ -2,13 +2,17 @@ use std::ptr::NonNull;
 
 use crate::{
     ops::place::{
+        BorrowPlace,
         CreateHandle,
         PlaceHandle,
         PlaceProxy,
         ProjectPlace,
+        ReadPlace,
+        WritePlace,
         borrowck::{
             AccessKind,
             Instant,
+            Lifetime,
         },
     },
     place::Subplace,
@@ -39,5 +43,73 @@ unsafe impl<S: Subplace> ProjectPlace<S> for NonNull<S::Source> {
         let ptr: *mut S::Source = self.as_ptr();
         let ptr: *mut S::Target = unsafe { ptr.project_place(subplace) };
         unsafe { NonNull::new_unchecked(ptr) }
+    }
+}
+
+unsafe impl<'a, T: ?Sized> BorrowPlace<&'a T> for NonNull<T> {
+    const ACCESS: AccessKind = AccessKind::Shared;
+    type Timing = Lifetime<'a>;
+    const SAFE: bool = false;
+
+    unsafe fn borrow(self) -> &'a T {
+        unsafe { self.as_ref() }
+    }
+}
+
+unsafe impl<'a, T: ?Sized> BorrowPlace<&'a mut T> for NonNull<T> {
+    const ACCESS: AccessKind = AccessKind::Exclusive;
+    type Timing = Lifetime<'a>;
+    const SAFE: bool = false;
+
+    unsafe fn borrow(mut self) -> &'a mut T {
+        unsafe { self.as_mut() }
+    }
+}
+
+unsafe impl<T: ?Sized> BorrowPlace<*const T> for NonNull<T> {
+    const ACCESS: AccessKind = AccessKind::Untracked;
+    type Timing = Instant;
+    const SAFE: bool = true;
+
+    unsafe fn borrow(self) -> *const T {
+        self.as_ptr()
+    }
+}
+
+unsafe impl<T: ?Sized> BorrowPlace<*mut T> for NonNull<T> {
+    const ACCESS: AccessKind = AccessKind::Untracked;
+    type Timing = Instant;
+    const SAFE: bool = true;
+
+    unsafe fn borrow(self) -> *mut T {
+        self.as_ptr()
+    }
+}
+
+unsafe impl<T: ?Sized> BorrowPlace<NonNull<T>> for NonNull<T> {
+    const ACCESS: AccessKind = AccessKind::Untracked;
+    type Timing = Instant;
+    const SAFE: bool = true;
+
+    unsafe fn borrow(self) -> NonNull<T> {
+        self
+    }
+}
+
+unsafe impl<T> WritePlace for NonNull<T> {
+    const ACCESS: AccessKind = AccessKind::Exclusive;
+    const SAFE: bool = false;
+
+    unsafe fn write_place(self, value: Self::Target) {
+        unsafe { self.write(value) }
+    }
+}
+
+unsafe impl<T> ReadPlace for NonNull<T> {
+    const ACCESS: AccessKind = AccessKind::Exclusive;
+    const SAFE: bool = false;
+
+    unsafe fn read_place(self) -> Self::Target {
+        unsafe { self.read() }
     }
 }
